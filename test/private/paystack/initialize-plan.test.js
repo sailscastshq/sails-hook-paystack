28b9e37249f1872227f8885504a550f742623a32
Kelvin Omereshone <kelvinomereshone@gmail.com>
2021-02-10 11:38:07 +0100

    test: :white_check_mark: remove .only

--- test/private/paystack/initialize-plan.test.js
+++ test/private/paystack/initialize-plan.test.js
@@ -1,5 +1,5 @@
 describe('sails.helpers.initializeTransaction()', () => {
-  it.only('Successfully initialize Transaction', async () => {
+  it('Successfully initialize Transaction', async () => {
     await global.sails.helpers.paystack.initializeTransaction.with({
       email: 'customer@email.com',
       amount: 100
